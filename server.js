a5d420d66ea43a476fa4e0df452dc7aa6895b973
纸船 <63154933+paper-boat-lzc@users.noreply.github.com>
2026-06-17 14:47:13 +0800

    sec

--- server.js
+++ server.js
@@ -632,7 +632,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 server.listen(PORT, () => {
   console.log(`
 ╔══════════════════════════════════════════════════╗
